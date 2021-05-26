d1c9dc91509c34991b3df0514e0dedc1cb726265
Aaron Elkins <threcius@yahoo.com>
2021-05-26 15:18:33 +0800

    Automatically set the right version for setup.py

---
+++ caprice/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding=utf-8 -*-
+from .version import VERSION
+
+__version__ = VERSION
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 """
 Caprice is a robust PDF generation library for Python
 """
-
+import os
 from setuptools import find_packages, setup
+
+def get_version():
+    basedir = os.path.dirname(__file__)
+    try:
+        with open(os.path.join(basedir, 'caprice/version.py')) as f:
+            locals = {}
+            exec(f.read(), locals)
+            return locals['VERSION']
+    except FileNotFoundError:
+        raise RuntimeError('No version info found.')
 
 setup(
     name='caprice',
     packages=find_packages(include=['caprice']),
-    version='0.1.1',
+    version=get_version(),
     url='https://github.com/orklann/caprice',
     description='Robust PDF generation library for Python',
     long_description=__doc__,
