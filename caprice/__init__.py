69a64def9dd8d80b518f5dc2a8cb9db490fe68a4
Aaron Elkins <threcius@yahoo.com>
2021-05-29 12:57:07 +0800

    Introducing Document class

--- caprice/__init__.py
+++ caprice/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding=utf-8 -*-
 from .version import VERSION
+from .document import Document
 
 __version__ = VERSION
---
+++ caprice/document.py
@@ -0,0 +1,7 @@
+# -*- coding=utf-8 -*-
+
+class Document:
+    def __init__(self) -> None:
+        print("Introducing Document!")
+        self.pages = []
+        
