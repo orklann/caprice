ed365666c896eb49989713fbdf405a468fff8265
Aaron Elkins <threcius@yahoo.com>
2021-05-29 16:14:41 +0800

    Add comments

--- caprice/document.py
+++ caprice/document.py
@@ -18,8 +18,12 @@ class Document:
         return p
 
     def add_indirect(self, indirect: GIndirect):
+        """Add GIndirect to Document, by using increasing object number
+        """
+        # Set the tracking object number
         indirect.set_obj_num(self.indirect_obj_num_count)
         indirect.set_generation_num(0)
+        # Increse tracking object number
         self.indirect_obj_num_count += 1
         key = indirect.get_ref().compile_str()
         self.indirects_dict[key] = indirect
