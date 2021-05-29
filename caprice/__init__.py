84ffce3359890128ec8224cf9398b13c729a3df4
Aaron Elkins <threcius@yahoo.com>
2021-05-29 12:58:23 +0800

    Introducing Page class

--- caprice/__init__.py
+++ caprice/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 from .version import VERSION
 from .document import Document
+from .page import Page
 
 __version__ = VERSION
---
+++ caprice/page.py
@@ -0,0 +1,5 @@
+# -*- coding=utf-8 -*-
+
+class Page:
+    def __init__(self) -> None:
+        print("Introducing Page!")
