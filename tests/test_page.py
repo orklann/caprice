8bff8dec187d5c679c552699347841c26f3fec99
Aaron Elkins <threcius@yahoo.com>
2021-06-02 15:35:33 +0800

    Add test_indirect_obj() to test_page.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -2,6 +2,10 @@
 
 import unittest
 from caprice.page import Page
+from caprice.document import Document
 
 class TestPage(unittest.TestCase):
-    pass
+    def test_indirect_obj(self):
+        doc = Document()
+        page = Page(doc)
+        self.assertEqual(page.indirect_obj.get_ref_str(), "1 0 R")
