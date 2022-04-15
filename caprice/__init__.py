86ae01d61065bf2db806a50ec980c41fd94ca71e
Aaron Elkins <threcius@yahoo.com>
2022-04-15 12:24:57 +0800

    Add config.py

--- caprice/__init__.py
+++ caprice/__init__.py
@@ -1,3 +1,9 @@
 from .version import VERSION
+import sys
+import os
 
 __version__ = VERSION
+
+# We need to import some modules in the root form other parts of the library.
+# Like config.py, etc
+sys.path.append(os.path.join(os.getcwd(), "caprice")) 
---
+++ caprice/config.py
@@ -0,0 +1,4 @@
+import os
+
+name = "Caprice"
+data_dir = os.path.join(os.getcwd(), "data")
