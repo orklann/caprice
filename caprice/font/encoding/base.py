d138696b4ec47605b624a714b00320f160e16d96
Aaron Elkins <threcius@yahoo.com>
2022-04-19 10:44:38 +0800

    Add some mapping methods to base encoding class

--- caprice/font/encoding/base.py
+++ caprice/font/encoding/base.py
@@ -5,4 +5,31 @@ class Encoding:
     
     def __init__(self):
         self.code_to_name = {}
-        
+        self.encoding_name = None
+
+    def name(self, code):
+        """Return the glyph name for the given character code, if not found,
+        just return .notdef
+        """
+        # TODO: Add unit test for this method
+        self.code_to_name.get(code, ".notdef")
+
+    def unicode(self, code):
+        """Return the Unicode value in UTF-8 for the given code, return None 
+        if not found.
+        """
+        # TODO: Add unit test for this method
+        return GlyphList.name_to_unicode(self.name(code))
+
+    def code(self, name):
+        """Return the character code for the given glyph name, return None if 
+        not found.
+
+        Note: If multiple character codes maps to the same given glyph name, the
+        first found is returned.
+        """
+        # TODO: Add unit test for this method
+        for c in self.code_to_name:
+            if self.code_to_name[c] == name:
+                return c
+            return None
