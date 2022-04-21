62b9afe66af2f8927c36dc7701ae52580cd30028
Aaron Elkins <threcius@yahoo.com>
2022-04-21 10:44:12 +0800

    Add difference in encoding

--- caprice/font/encoding/base.py
+++ caprice/font/encoding/base.py
@@ -5,13 +5,31 @@ class Encoding:
     
     def __init__(self):
         self.code_to_name = {}
+        # Difference id dynamically created 
+        # NOTE: To make difference, we need to call `code_to_unicode` first, 
+        #       before calling other methods.
+        self.diff_code_to_name = {}
         self.encoding_name = None
+
+    def _diff_code_from_unicode(self, unicode):
+        name = self.unicode_to_name(unicode)
+        for c in range(1, 256):
+            if c not in self.code_to_name and \
+                    c not in self.diff_code_to_name:
+                self.diff_code_to_name[c] = name
+                return c
+        return None
+
 
     def name(self, code):
         """Return the glyph name for the given character code, if not found,
         just return .notdef
         """
-        return self.code_to_name.get(code, ".notdef")
+        glyph_name = self.code_to_name.get(code, ".notdef")
+        if glyph_name == ".notdef":
+            return self.diff_code_to_name.get(code, ".notdef")
+        else:
+            return glyph_name
 
     def unicode(self, code):
         """Return the Unicode value in UTF-8 for the given code, return None 
@@ -29,13 +47,20 @@ class Encoding:
         for c in self.code_to_name:
             if self.code_to_name[c] == name:
                 return c
+        for c in self.diff_code_to_name:
+            if self.diff_code_to_name[c] == name:
+                return c
         return None
 
     def code_from_unicode(self, unicode):
         """Return the character code for the given unicode, return None if not
         found.
         """
-        return self.code(self.unicode_to_name(unicode))
+        code = self.code(self.unicode_to_name(unicode))
+        if code is None:
+            return self._diff_code_from_unicode(unicode)
+        else:
+            return code
 
     def unicode_to_name(self, unicode):
         """Return glyph name for the given unicode by looking the Adobe Glyph List."""
--- tests/font/encoding/test_win_ansi_encoding.py
+++ tests/font/encoding/test_win_ansi_encoding.py
@@ -10,21 +10,37 @@ class TestWinAnsiEncoding(unittest.TestCase):
         win = WinAnsiEncoding()
         self.assertEqual(win.name(0o101), "A")
         self.assertEqual(win.name(0o255), "hyphen")
+        # Make sure to call code_from_unicode() to construct difference
+        c1 = win.code_from_unicode("˚")
+        self.assertEqual(c1, 1)
+        self.assertEqual(win.name(c1), "ring")
  
     def test_code(self):
         """Test base class's code() method"""
         win = WinAnsiEncoding()
         self.assertEqual(win.code("ydieresis"), 0o377)
         self.assertEqual(win.code("hyphen"), 0o055)
+        # Make sure to call code_from_unicode() to construct difference
+        c1 = win.code_from_unicode("˚")
+        self.assertEqual(win.code("ring"), c1)
 
     def test_unicode(self):
         """Test base class's unicode() method"""
         win = WinAnsiEncoding()
         self.assertEqual(win.unicode(0o256), "®")
         self.assertEqual(win.unicode(0o200), "€")
+        # Make sure to call code_from_unicode() to construct difference
+        c1 = win.code_from_unicode("˚")
+        self.assertEqual(win.unicode(c1), "˚")
+
 
     def test_code_from_unicode(self):
         """Test base class's code_from_unicode() method"""
         win = WinAnsiEncoding()
         self.assertEqual(win.code_from_unicode("®"), 0o256)
         self.assertEqual(win.code_from_unicode("€"), 0o200)
+        # Make sure to call code_from_unicode() to construct difference
+        c1 = win.code_from_unicode("˚")
+        self.assertEqual(c1, 1)
+        c2 = win.code_from_unicode("ı")
+        self.assertEqual(c2, 2)
